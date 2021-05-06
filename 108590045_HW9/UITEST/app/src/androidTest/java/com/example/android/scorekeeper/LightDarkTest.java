2be83111f7bd758378dd0c098e903d45bf1ab8a3
root <t108590045@ntut.edu.tw>
2021-05-06 18:24:08 +0800

    06-3

---
+++ 108590045_HW9/UITEST/app/src/androidTest/java/com/example/android/scorekeeper/LightDarkTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.scorekeeper;
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Recorded Espresso test that tests the score plus and minus buttons
+ * and results for Team 1.
+ */
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class LightDarkTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule
+            = new ActivityTestRule<>(MainActivity.class);
+
+    /**
+     * Tests the score plus and minus buttons.
+     */
+    @Test
+    public void LightDarkTest() {
+        for (int i=0;i<=5;i++){
+            onView(withContentDescription("More options")).perform(click());
+            onView(allOf(withText("Night Mode"),isDisplayed())).perform(click());
+            onView(withContentDescription("More options")).perform(click());
+            onView(allOf(withText("Day Mode"),isDisplayed())).perform(click());
+        }
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position "
+                        + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup
+                        && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent)
+                        .getChildAt(position));
+            }
+        };
+    }
+}
